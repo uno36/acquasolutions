4617449c74c6eb977edc63e378cfd8d7b6df9867
Emmanuel Tofa <emmanuelutofa@gmail.com>
2024-02-10 02:03:36 +0200

    change

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact/>} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact/>} />
           </Route>
         </Routes>        
       </BrowserRouter>
